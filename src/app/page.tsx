b79f867d929d320d7c2177d22126f5cf3e0ff89c
Keanu Tibule <keanudane@yahoo.com>
2024-05-10 13:34:06 +0800

    Fixed apostrophe deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import Navbar from "@/app/components/Navbar";
 import SocIcons from "./components/SocIcons";
 import Footer from "./components/Footer";
@@ -10,12 +9,12 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center min-h-screen">
         <Navbar />
         <div className="text-center my-8">
-          <h1 className="text-9xl font-bold">Hi, I'm Keanu.</h1>
+          <h1 className="text-9xl font-bold">Hi, I&apos;m Keanu.</h1>
         </div>
         <div className="text-center my-4">
           <h2 className="text-zinc-500">
             A passionate programmer dedicated to crafting innovative solutions
-            through code. Let's turn ideas into reality!
+            through code. Let&apos;s turn ideas into reality!
           </h2>
         </div>
         <SocIcons />

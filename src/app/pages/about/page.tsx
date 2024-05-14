8c134b44f198327bad244685d4d594903eb379e9
Keanu Tibule <keanudane@yahoo.com>
2024-05-14 16:38:52 +0800

    About Page Creation - About page in progress - AboutSidebar in progress

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,8 +1,21 @@
+import AboutCard from '@/app/components/AboutCard';
+import AboutSidebar from '@/app/components/AboutSidebar'
+import Footer from '@/app/components/Footer';
+import Navbar from '@/app/components/Navbar';
 import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      {/* <AboutSidebar /> */}
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <AboutCard />
+      </div>
+      <Footer />
+    </>
   )
 }
 export default About;
